d212bf5b94209ebf7b4223ae33e1f4e2d6d05548
Patrick Everitt <patrick.everitt@pipedrive.com>
2022-06-04 20:01:59 +0200

    Added letters level progression

--- src/components/games/letters/letters.jsx
+++ src/components/games/letters/letters.jsx
@@ -5,58 +5,115 @@ import images from './images'
 import './letters.css'
 
 const DEFAULT_SETTINGS = {
-  lettersClicked: 0,
-  mistakes: 0,
-  starCount: 5,
-  gameReady: false,
-  imageCount: 0,
-  levelNumber: 0
+  levelNumber: 0,
+  letterIndex: 0,
+  lettersClicked: '',
+  starCount: 5
 }
 
 // create some variables to store our images
-let bgImage
 let star
-const letters = {}
+const letterImages = {}
+const bgImages = {}
 
 export const Letters = () => {
   const [settings, setSettings] = useState(DEFAULT_SETTINGS)
 
+  const preload = (p5) => {
+    star = p5.loadImage(images.star)
+    loadAllImages(p5)
+    loadLevel(settings.levelNumber)
+  }
+
+  const loadAllImages = (p5) => {
+    for (let i = 0; i < levels.length; i++) {
+      const { letters } = levels[i]
+      const backgroundUrl = images[levels[i].background]
+
+      bgImages[i] = p5.loadImage(backgroundUrl)
+
+      for (let j = 0; j < letters.length; j++) {
+        const character = letters[j].character
+
+        if (character && images[character]) {
+          letterImages[character] = p5.loadImage(images[character])
+        }
+      }
+    }
+  }
+
+  const loadLevel = (levelNumber) => {
+    console.log(`loading level ${levelNumber}`)
+    setSettings({ ...DEFAULT_SETTINGS, ...{ levelNumber } })
+  }
+
   const setup = (p5, canvasParentRef) => {
     // this creates a canvas of a certain size
-    p5.createCanvas(400, 400).parent(canvasParentRef)
+    p5.createCanvas(400, 440).parent(canvasParentRef)
   }
 
   const draw = (p5) => {
-    const drawLetter = () => {
-      // draw the letter at the position
-      p5.imageMode(p5.CENTER)
+    const { levelNumber } = settings
 
-      for (let i = 0; i < levels[settings.levelNumber].letters.length; i++) {
-        const letter = levels[settings.levelNumber].letters[i]
+    p5.background('#fae2e2')
 
-        p5.image(
-          letters[letter.character],
-          letter.x,
-          letter.y,
-          letter.radius * 2,
-          letter.radius * 2
-        )
-      }
-    }
-
-    p5.background(0)
+    // set p5 to draw images using (x,y) as corner
     p5.imageMode(p5.CORNER)
-    let x = 200
-    for (let i = 0; i < settings.starCount; i++) {
-      p5.image(star, x, 5, 20, 20)
-      x = x + 30
+    p5.image(bgImages[levelNumber], 0, 40)
+
+    drawStars(p5)
+    drawLetters(p5)
+    drawLettersClicked(p5)
+
+    if (isLevelFinished()) {
+      loadLevel(settings.levelNumber + 1)
     }
+  }
 
-    p5.image(bgImage, 0, 40)
+  const isLevelFinished = () => {
+    const { levelNumber, letterIndex } = settings
+    const { letters } = levels[levelNumber]
 
-    // call our function that draws the letter
-    drawLetter()
+    if (letterIndex === letters.length) {
+      return true
+    } else {
+      return false
+    }
+  }
 
+  const drawStars = (p5) => {
+    const { starCount } = settings
+    let starX = 200
+    const starY = 5
+
+    for (let i = 0; i < starCount; i++) {
+      p5.image(star, starX, starY, 20, 20)
+      starX = starX + 30
+    }
+  }
+
+  const drawLetters = (p5) => {
+    // set p5 to draw images using (x,y) as center
+    p5.imageMode(p5.CENTER)
+
+    const { levelNumber } = settings
+    const level = levels[levelNumber]
+
+    for (let i = 0; i < level.letters.length; i++) {
+      const letter = level.letters[i]
+      const letterImage = letterImages[letter.character]
+
+      p5.image(
+        letterImage,
+        letter.x,
+        letter.y,
+        letter.radius * 2,
+        letter.radius * 2
+      )
+    }
+  }
+
+  const drawLettersClicked = (p5) => {
     // set some properties for the text we will write on the canvas
     p5.textSize(20)
     p5.noStroke()
@@ -66,28 +123,11 @@ export const Letters = () => {
     p5.text(settings.lettersClicked, 20, 20)
   }
 
-  const preload = (p5) => {
-    loadLevel(0, p5)
-    star = p5.loadImage(images.star)
-  }
-
-  const loadLevel = (level, p5) => {
-    setSettings({ ...DEFAULT_SETTINGS, ...{ levelNumber: level } })
-
-    bgImage = p5.loadImage(images[levels[level].background])
-
-    for (let i = 0; i < levels[level].letters.length; i++) {
-      const character = levels[level].letters[i].character
-      if (character && images[character]) {
-        letters[character] = p5.loadImage(images[character])
-      }
-    }
-  }
-
   // runs when the mouse is clicked
   const mousePressed = (p5) => {
-  // calculate distance from mouse to center of letter
-    const targetLetter = levels[settings.levelNumber].letters[settings.lettersClicked]
+    const { levelNumber, letterIndex } = settings
+    const { letters } = levels[levelNumber]
+    const targetLetter = letters[letterIndex]
 
     const distanceToMouse = p5.floor(
       p5.dist(p5.mouseX, p5.mouseY, targetLetter.x, targetLetter.y)
@@ -96,21 +136,25 @@ export const Letters = () => {
     // if distance bigger than radius, player missed
     if (distanceToMouse > targetLetter.radius) {
       console.log('miss')
-      setSettings({ ...settings, ...{ mistakes: settings.mistakes + 1, starCount: settings.starCount - 1 } })
-      console.log('pocet chyb' + settings.mistakes)
+      decreaseStars()
     } else {
       console.log('hit')
-      const lettersClicked = settings.lettersClicked + 1
-      setSettings({ ...settings, ...{ lettersClicked } })
-
-      if (lettersClicked === levels[settings.levelNumber].letters.length) {
-        console.log(
-          'jsi na konci a ted tu chci vymazat vsechny promenne a naplnit je necim novym'
-        )
-
-        loadLevel(settings.levelNumber + 1)
-      }
+      increaseWord()
     }
+  }
+
+  const decreaseStars = () => {
+    setSettings({ ...settings, ...{ starCount: settings.starCount - 1 } })
+    console.log(`pocet chyb ${settings.starCount - 5}`)
+  }
+
+  const increaseWord = () => {
+    const { levelNumber, letterIndex } = settings
+    const { letters } = levels[levelNumber]
+    const targetLetter = letters[letterIndex]
+
+    const lettersClicked = settings.lettersClicked + targetLetter.character
+    setSettings({ ...settings, ...{ lettersClicked }, ...{ letterIndex: letterIndex + 1 } })
   }
 
   return (
