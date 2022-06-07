901f14072a93fc74a9a075a5344088374ad8874c
BukovskaPavla <bukovskapavla@gmail.com>
2022-06-07 14:12:10 +0200

    more levels in directions and turnoff debugMode in letters

--- src/components/games/directions/images.jsx
+++ src/components/games/directions/images.jsx
@@ -4,6 +4,14 @@ import PL_1_C from 'img/PL_1_C.png'
 import PL_1_D from 'img/PL_1_D.png'
 import PL_1_E from 'img/PL_1_E.png'
 import PL_1_F from 'img/PL_1_F.png'
+import PL_2_B from 'img/PL_2_B.png'
+import PL_2_B_X from 'img/PL_2_B_X.png'
+import PL_2_D from 'img/PL_2_D.png'
+import PL_2_D_X from 'img/PL_2_D_X.png'
+import PL_3_1 from 'img/PL_3_1.png'
+import PL_3_1_X from 'img/PL_3_1_X.png'
+import PL_4 from 'img/PL_4.png'
+import PL_4_X from 'img/PL_4_X.png'
 import sadElephant from 'img/sad-elephant.png'
 import bodyElephant from 'img/elephant-body.png'
 import fruit from 'img/banan.png'
@@ -15,6 +23,14 @@ export default {
   PL_1_D,
   PL_1_E,
   PL_1_F,
+  PL_2_B,
+  PL_2_B_X,
+  PL_2_D,
+  PL_2_D_X,
+  PL_3_1,
+  PL_3_1_X,
+  PL_4,
+  PL_4_X,
   sadElephant,
   fruit,
   bodyElephant
